baaa0a4cb3a3117aeea3b2ebadcb574fe9874340
poposkiognen <ognen.p@outlook.com>
2018-11-11 18:59:12 +0100

    Port changes

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var hbs = exphbs.create({
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.set('port', 80);
+app.set('port', process.env.PORT || 5000);
 
 app.use('/views/css/', express.static(__dirname + '/views/css/'));
 app.use('/views/css/page', express.static(__dirname + '/views/css/page/'));
